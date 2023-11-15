6ec8afd7c5652a5bff6cc8a49429fe1340ba66eb
Aconnollyodu <146770995+Aconnollyodu@users.noreply.github.com>
2023-11-14 19:04:40 -0500

    Add files via upload

---
+++ my scripts/Enemy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+
+	public GameObject[] WaveEnemies;
+	public GameObject[] BigEnemies;
+	public GameObject[] FastEnemies;
+	public GameObject[] Enemies;
+	public List<GameObject> enemies;
+
+	public float waitTime;
+
+}
